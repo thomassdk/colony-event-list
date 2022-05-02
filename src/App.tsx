fb42a947b52b52a5ea36af756dbf0e207133dfc4
thomassdk <thomassdk@pm.me>
2022-05-02 19:19:18 +0100

    App Component

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from 'react'
+
+import styles from './App.module.css'
+import { getColonyEvents } from './Colony'
+import EventList from './components/EventList'
+import { TEvents } from './interfaces'
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [events, setEvents] = useState<TEvents[]>([]);
+
+    useEffect(() => {
+        async function fetch() {
+            const events = await getColonyEvents();
+            setEvents(events);
+        }
+
+        fetch()
+    }, []);
+
+    return (
+        <div className={styles.App}>
+            <EventList events={events} />
+        </div>
+    );
 }
 
 export default App;
